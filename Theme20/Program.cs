e5ca03966726ca9a59f8486bcd421982aec3f8f3
ASUS <ASUS@DESKTOP-QOB7I2F>
2024-07-18 19:52:53 +0300

    Добавьте файлы проекта.

---
+++ Theme20/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("Введите число: ");
+int num;
+int.TryParse(Console.ReadLine(), out num);
+Console.WriteLine("Выберите операцию:");
+Console.WriteLine("1. Логическое побитовое умножение");
+Console.WriteLine("2. Логическое побитовое сложение");
+Console.WriteLine("3. Шифрование по ключу");
+Console.WriteLine("4. Инверсия");
+int choice = int.Parse(Console.ReadLine()!);
+int Result()
+{
+    int BitwiseAnd(int a, int b)
+    {
+        return a & b;
+    }
+
+    int BitwiseOr(int a, int b)
+    {
+        return a | b;
+    }
+
+    int Encrypt(int a, int key)
+    {
+        return a ^ key;
+    }
+
+    int Invert(int a)
+    {
+        return ~a;
+    }
+
+    switch (choice)
+    {
+        case 1:
+            Console.WriteLine("Введите второе число для побитового умножения:");
+            int b1 = int.Parse(Console.ReadLine());
+            return BitwiseAnd(num, b1);
+        case 2:
+            Console.WriteLine("Введите второе число для побитового сложения:");
+            int b2 = int.Parse(Console.ReadLine());
+            return BitwiseOr(num, b2);
+        case 3:
+            Console.WriteLine("Введите ключ для шифрования:");
+            int key = int.Parse(Console.ReadLine());
+            return Encrypt(num, key);
+        case 4:
+            return Invert(num);
+        default:
+            Console.WriteLine("Неверный выбор операции.");
+            return 0;
+    }
+}
+int result = Result();
+Console.WriteLine($"Результат: {result}");
---
+++ Theme25/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Theme25
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            label4 = new Label();
+            linkLabel1 = new LinkLabel();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(70, 159);
+            label1.Name = "label1";
+            label1.Size = new Size(119, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(12, 191);
+            label2.Name = "label2";
+            label2.Size = new Size(235, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Fell less stressed and more minful ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(79, 211);
+            label3.Name = "label3";
+            label3.Size = new Size(97, 17);
+            label3.TabIndex = 2;
+            label3.Text = "with meditation";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaptionText;
+            button1.ForeColor = SystemColors.ButtonFace;
+            button1.Location = new Point(89, 250);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Login ";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Location = new Point(12, 331);
+            label4.Name = "label4";
+            label4.Size = new Size(131, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Don't have an account?";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.BackColor = Color.Transparent;
+            linkLabel1.Location = new Point(160, 331);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(47, 15);
+            linkLabel1.TabIndex = 5;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Sign up";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(254, 416);
+            Controls.Add(linkLabel1);
+            Controls.Add(label4);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Тема 25, 2/8";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Label label4;
+        private LinkLabel linkLabel1;
+    }
+}
